7c113dc04da9596b6736c4cf251b23a465fe1a66
edwin141999 <gendevilman@gmail.com>
2023-07-10 19:26:09 -0600

    api-portfolio

---
+++ server.js
@@ -0,0 +1,16 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db/db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(jsonServer.rewriter({
+  "/api/*": "/$1",
+}))
+server.use(router)
+
+server.listen(3000, () => {
+  console.log('JSON Server is running on port 3000')
+})
+
+module.exports = server
